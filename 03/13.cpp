5ea1f8da8563bcb6d742f8282df87d83da8d0a38
Ajinkya Deshmukh <ajinkya@Ajinkyas-MacBook-Air.local>
2018-01-09 09:12:59 +0530

    exercise 3.13

--- 03/13.cpp
+++ 03/13.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main(){
-	std::cout << "Hello world!\n";
 	vector<int> ivec(10);
-	for(int i=0;i<10 ; i++){
-		
+	vector<int>::size_type ix;
+	for(ix=0;ix!=ivec.size();++ix){
+		cin >> ivec[ix];
+	}
+	cout << "Ivec size is: " << ivec.size() << "\n";
+	for(ix=0;ix!=ivec.size();++ix){
+		if(ivec[ix]<ivec.size()){
+			cout<< ivec[ix]+ivec[ix+1] << '\n';
+		}
 	}
 }
